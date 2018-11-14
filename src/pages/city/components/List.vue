87ccc6f2e1fedc3b6eb6f965d2cddf5dc432e123
liuzhuang <15062203198@139.com>
2018-11-14 15:57:49 +0800

    add city-list

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="list-title border-topbottom">
+        您的位置
+      </div>
+      <div class="list-button">
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="list-title border-topbottom">
+        热门城市
+      </div>
+      <div class="list-button">
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wraper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="list-title border-topbottom">
+        A
+      </div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+      <div class="list-title border-topbottom">
+        A
+      </div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+      <div class="list-title border-topbottom">
+        A
+      </div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    position: absolute
+    overflow: hidden
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .list-title
+      height: .64rem
+      line-height: .64rem
+      background-color: #eee
+      padding: 0 .1rem
+    .list-button
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wraper
+        float: left
+        width: 33.33%
+        .button
+          margin: .1rem
+          text-align:center
+          border: .02rem solid green
+          padding: .1rem
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
+</style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" placeholder='请输入城市名或拼音'/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .search
+    height: .72rem
+    background-color: $bgcolor
+    padding: 0 .1rem
+    text-align: center
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      line-height: .64rem
+      text-align: center
+      border-radius: .06rem
+      padding: 0 .1rem
+      color: #666
+</style>
